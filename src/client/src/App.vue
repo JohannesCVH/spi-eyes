48f8bf884a2b0072d6ab59fc5ab78fcafab1add0
Johannes van Heerden <johannescvh@icloud.com>
2026-01-08 18:30:45 +0200

    Added logos I generated with AI

--- src/client/src/App.vue
+++ src/client/src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <nav class="navbar navbar-light bg-light mb-4">
       <div class="container-fluid">
-        <router-link class="navbar-brand" to="/">Spi-Eyes</router-link>
+        <!-- <router-link class="navbar-brand" to="/">Spi-Eyes</router-link> -->
+        <router-link class="navbar-brand" to="/">
+          <img src="./assets/logo.png" style="max-width: 48px; box-shadow: 0 4px 8px rgba(0,0,0,0.2); border-radius: 4px;"/>
+        </router-link>
       </div>
     </nav>
   <div class="container">
